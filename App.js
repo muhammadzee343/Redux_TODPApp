b64849a203cb986de9d7f69b67e34acf0600812a
muhammadzee343 <muhammad.zee343@gmail.com>
2020-07-27 11:53:28 +0500

    fresh project

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Redux Todo</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
---
+++ android/app/src/main/java/com/reduc_todoapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.reduc_todoapp;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Reduc_ToDoApp";
+  }
+}
